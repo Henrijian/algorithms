a450b16fe44c5fe3fe243c83b4aeafb7cbb10b5b
Henri Jian <henrijian@gmail.com>
2021-07-07 18:08:09 +0800

    add BinaryStdIn client class - BinaryDump.

---
+++ strings/data_compression/BinaryDump.java
@@ -0,0 +1,69 @@
+/******************************************************************************
+ *  Compilation:  javac BinaryDump.java
+ *  Execution:    java BinaryDump n < file
+ *  Dependencies: BinaryStdIn.java
+ *
+ *  Reads in a binary file and writes out the bits, n per line.
+ *
+ *  % more abra.txt
+ *  ABRACADABRA!
+ *
+ *  % java BinaryDump 16 < abra.txt
+ *  0100000101000010
+ *  0101001001000001
+ *  0100001101000001
+ *  0100010001000001
+ *  0100001001010010
+ *  0100000100100001
+ *  96 bits
+ *
+ ******************************************************************************/
+package strings.data_compression;
+
+/**
+ *  The {@code BinaryDump} class provides a client for displaying the contents
+ *  of a binary file in binary.
+ *  <p>
+ *  For more full-featured versions, see the Unix utilities
+ *  {@code od} (octal dump) and {@code hexdump} (hexadecimal dump).
+ */
+public class BinaryDump {
+    private BinaryDump() {
+        // Do not instantiate.
+    }
+
+    /**
+     * Reads in a sequence of bytes from standard input and writes
+     * them to standard output in binary, k bits per line,
+     * where k is given as a command-line integer (defaults
+     * to 16 if no integer is specified); also writes the number
+     * of bits.
+     *
+     * @param argv the command-line arguments
+     */
+    public static void main(String[] argv) {
+        int bitsPerLine = 16;
+        if (argv.length == 1) {
+            bitsPerLine = Integer.parseInt(argv[0]);
+        }
+
+        int count;
+        for (count = 0; !BinaryStdIn.isEmpty(); count++) {
+            if (bitsPerLine == 0) {
+                BinaryStdIn.readBoolean();
+                continue;
+            } else if (count != 0 && count % bitsPerLine == 0) {
+                System.out.println();
+            }
+            if (BinaryStdIn.readBoolean()) {
+                System.out.print(1);
+            } else {
+                System.out.print(0);
+            }
+        }
+        if (bitsPerLine != 0) {
+            System.out.println();
+        }
+        System.out.println(count + " bits");
+    }
+}
